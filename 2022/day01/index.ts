463abfd156c6d76580a131a3f57342d37f7f5606
Eric Ventor <ericventor97@gmail.com>
2022-12-02 11:22:45 -0800

    linter and linting

--- 2022/day01/index.ts
+++ 2022/day01/index.ts
@@ -1,30 +1,30 @@
-import input from "./input";
-import testInput from "./test";
+import input from './input';
+import testInput from './test';
 
 function main(input: string): number[] {
-  const foodItemsCalories: string[] = input.split("\n");
+  const foodItemsCalories: string[] = input.split('\n');
   const caloriesPerElf: number[] = [0];
 
-  foodItemsCalories.forEach(foodItemCalories => {
+  foodItemsCalories.forEach((foodItemCalories) => {
     if (foodItemCalories !== '') {
       caloriesPerElf[caloriesPerElf.length - 1] += Number.parseInt(foodItemCalories, 10);
     } else {
-      caloriesPerElf.push(0)
+      caloriesPerElf.push(0);
     }
-  })
+  });
 
   return caloriesPerElf;
 }
 
-const test = () => main(testInput)
+const test = () => main(testInput);
 const partOne = () => Math.max(...main(input));
 const partTwo = (topN: number) => {
   const caloriesPerElf = main(input);
-  const sortedCaloriesPerElf = caloriesPerElf.sort((a,b) => b - a);
+  const sortedCaloriesPerElf = caloriesPerElf.sort((a, b) => b - a);
   const topNSum = sortedCaloriesPerElf.slice(0, topN).reduce((prev, curr) => curr + prev, 0);
   return topNSum;
-}
+};
 
-console.log("test", test());
-console.log("partOne", partOne());
-console.log("partTwo", partTwo(3));
+console.log('test', test());
+console.log('partOne', partOne());
+console.log('partTwo', partTwo(3));
